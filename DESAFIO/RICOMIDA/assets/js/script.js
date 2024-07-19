791b7409f2db368ccaa16a1ed3e56df9e448dc73
Rodolfo Cantillana <rodolfo.cantillana@gmail.com>
2024-07-18 20:56:26 -0400

    final

--- DESAFIO/RICOMIDA/assets/js/script.js
+++ DESAFIO/RICOMIDA/assets/js/script.js
@@ -4,3 +4,59 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 // tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+$(ingredientes).click(function() {
+    $(this).css({'color': 'red'});
+});
+
+$(preparacion).click(function() {
+    $(this).css({'color': 'red'});
+});
+
+$('#enviarcorreo').click(function () {
+    alert("Su correo fue enviado exitosamente");
+    
+});
+
+
+$('#titulocard1').dblclick(function () {
+    $(this).toggle("slow");
+    $('#imagencard1').toggle("slow");
+    $('#cuerpocard1').toggle("slow");
+    $('#imagencard2').toggle("slow");
+    $('#titulocard2').toggle("slow");
+    $('#cuerpocard2').toggle("slow");
+    $('#imagencard3').toggle("slow");
+    $('#titulocard3').toggle("slow");
+    $('#cuerpocard3').toggle("slow");
+    
+});
+
+$('#titulocard2').dblclick(function () {
+    $(this).toggle("slow");
+    $('#imagencard1').toggle("slow");
+    $('#titulocard1').toggle("slow");
+    $('#cuerpocard1').toggle("slow");
+    $('#imagencard2').toggle("slow");
+    $('#cuerpocard2').toggle("slow");
+    $('#imagencard3').toggle("slow");
+    $('#titulocard3').toggle("slow");
+    $('#cuerpocard3').toggle("slow");
+});
+
+$('#titulocard3').dblclick(function () {
+    $(this).toggle("slow");
+    $('#imagencard1').toggle("slow");
+    $('#titulocard1').toggle("slow");
+    $('#cuerpocard1').toggle("slow");
+    $('#imagencard2').toggle("slow");
+    $('#titulocard2').toggle("slow");
+    $('#cuerpocard2').toggle("slow");
+    $('#imagencard3').toggle("slow");
+    $('#cuerpocard3').toggle("slow");
+   
+});
+
